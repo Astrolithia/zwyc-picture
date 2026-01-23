b008b604edbcae91d6b608656dad5111f3579a62
Acw <astrolithia@gmail.com>
2026-01-23 21:12:42 +0800

    圖片相關接口開發完畢

---
+++ src/main/java/com/zwickyc/zwycpicturebackend/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.zwickyc.zwycpicturebackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zwickyc.zwycpicturebackend.model.entity.Picture;
+
+/**
+ * @author shiro
+ * @description 针对表【picture(图片)】的数据库操作Mapper
+ * @createDate 2026-01-21 16:53:09
+ * @Entity generator.domain.Picture
+ */
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
